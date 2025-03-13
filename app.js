0aaadfd0471415e8fff81a53b0895ce8916824a9
Maguin <193973433+ThreatHunterSec@users.noreply.github.com>
2025-03-13 16:35:43 -0300

    Adiciona função para inserir amigos na lista

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 let amigos = []; // Array para armazenar os amigos
+
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    amigos.push(nome);
+    input.value = "";
+}
